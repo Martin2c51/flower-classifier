9c2203083824a09e6ae401d922fc5d7e4466c6b7
Martin2c51 <42279489+Martin2c51@users.noreply.github.com>
2021-12-01 19:38:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
-from fastai.vision.all import *
-from fastai.vision import open_image
+from fastai.vision.all import 
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -14,7 +13,7 @@ classes = learn.data.classes
 
 def predict_single(img_file):
     "function to take image and return prediction"
-    prediction = learn.predict(open_image(img_file))
+    prediction = learn.predict(load_image(img_file))
     probs_list = prediction[2].numpy()
     return {
         "category": classes[prediction[1].item()],
